02e2e8900f3d65b5eded916cdbddc1ec58fa6962
Candice <hsline918@gmail.com>
2024-07-21 14:43:58 +0800

    add /tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,43 +1,20 @@
+const { fonts } = require("./tailwind/fonts");
+const { shadows } = require("./tailwind/shadows");
+const { backgrounds } = require("./tailwind/backgrounds");
+const textRenderPlugin = require("./tailwind/plugins/textRender");
+
 /** @type {import('tailwindcss').Config} */
-const plugin = require("tailwindcss/plugin");
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      fontFamily: {
-        "noto-sans": [
-          '"Noto Sans"',
-          '"system - ui"',
-          '"-apple - system"',
-          "BlinkMacSystemFont",
-          '"Segoe UI"',
-          "Roboto",
-          "Oxygen",
-          "Ubuntu",
-          "Cantarell",
-          '"Open Sans"',
-          '"Helvetica Neue"',
-          '"sans - serif"',
-        ],
-      },
-      boxShadow: {
-        soft: "0px 1px 3px rgba(0, 0, 0, 0.1), 0px 1px 2px rgba(0, 0, 0, 0.06)",
-      },
-      backgroundImage: {
-        "gradient-148": "linear-gradient(140deg, #f9fafb 0%, #d2d6db 100%)",
-      },
+      ...fonts,
+      ...shadows,
+      ...backgrounds,
     },
     variants: {},
 
-    plugins: [
-      function ({ addUtilities }) {
-        const newUtilities = {
-          ".text-render-optimize": {
-            "text-rendering": "optimizeLegibility",
-          },
-        };
-        addUtilities(newUtilities, ["responsive", "hover"]);
-      },
-    ],
+    plugins: [textRenderPlugin],
   },
 };
