887cd6dfc7a48d1680435eeaaa496bb076c919c1
Candice <hsline918@gmail.com>
2024-07-23 00:01:45 +0800

    create monorepo

--- projects/01-testimonial-card/src/App.jsx
+++ projects/01-testimonial-card/src/App.jsx
@@ -1,27 +1,32 @@
 import "./App.css";
-import BlogCard from "./components/BlogCard";
+import TestimonialCard from "./components/TestimonialCard";
 
-const cardData = {
-  badgeText: "Interior",
-  blogTitle: "Top 5 Living Room Inspirations",
-  blogSummary:
-    "Curated vibrants colors for your living, make it pop & calm in the same time.",
-  src: "./assets/comfy-living.jpeg",
-  alt: "", //for future dynamic purpose
-};
+const cardData = [
+  {
+    userName: "Sarah Dole",
+    userHandle: "@sarahdole",
+    testimonial:
+      "I've been searching for high-quality abstract images for my design projects, and I'm thrilled to have found this platform. The variety and depth of creativity are astounding!",
+    src: "./assets/profile-thumbnail.jpeg",
+    alt: "", //for future dynamic purpose
+  },
+];
+
 export default function App() {
   return (
     <>
-      <div className="h-screen pt-[120px] px-[17.5px]">
-        <BlogCard
-          key={cardData.blogTitle}
-          badgeText={cardData.badgeText}
-          blogTitle={cardData.blogTitle}
-          blogSummary={cardData.blogSummary}
-          src={cardData.src}
-          alt={cardData.alt}
-        />
-      </div>
+      {cardData.map((card) => (
+        <div className="h-screen md:min-h-[1024px] lg:min-h-[768px] flex justify-center items-start pt-[200px] px-[17.5px]">
+          <TestimonialCard
+            key={card.userHandle}
+            userName={card.userName}
+            userHandle={card.userHandle}
+            testimonial={card.testimonial}
+            src={card.src}
+            alt={card.alt}
+          />
+        </div>
+      ))}
     </>
   );
 }
--- tailwind/backgrounds.js
+++
@@ -1,5 +0,0 @@
-export const backgrounds = {
-  backgroundImage: {
-    "gradient-148": "linear-gradient(140deg, #f9fafb 0%, #d2d6db 100%)",
-  },
-};
